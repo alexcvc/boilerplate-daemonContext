bb2c1cabcc61d48f3ae360b167873a0ec5292ef8
Alexander Sacharov <1076183+alexcvc@users.noreply.github.com>
2024-08-26 16:14:34 +0200

    Class TaskController  (#7)
    * Add task-abstracting CMake setup and TaskApp implementation
    
    Created CMakeLists.txt for the task-abstracting module with necessary dependencies and installation rules. Implemented TaskApp class with start and stop functionality, including thread management. Added taskApp initialization in main.cpp.
    Wip…
    
    * Class TaskController
    
    Removed TaskApp class and replaced it with a new Daemon class for handling daemon processes.
    Introduced TaskController for managing event-driven tasks with optional duration configuration.
    
    * Rename task-abstracting to task_controller and refactor.
    
    Reorganized project structure by renaming the `task-abstracting` directory to `task_controller`. Refactored the core functionality to use a new `ThreadController` class, improving thread management and scalability.

---
+++ src/daemon/daemonConfig.hpp
@@ -0,0 +1,32 @@
+/* SPDX-License-Identifier: MIT */
+//
+// Copyright (c) 2024 Alexander Sacharov <a.sacharov@gmx.de>
+//               All rights reserved.
+//
+// This work is licensed under the terms of the MIT license.
+// For a copy, see <https://opensource.org/licenses/MIT>.
+//
+
+#pragma once
+
+#include <string>
+
+namespace app {
+/**
+ * @struct DaemonConfig
+ *
+ * @brief The DaemonConfig struct represents the configuration for a daemon process.
+ *
+ * The DaemonConfig struct holds information such as the path of the PID file,
+ * whether the process should run as a daemon, and whether there is a test console
+ * running in the foreground.
+ */
+struct DaemonConfig {
+  std::string pidFile;           ///< The path of the PID file
+  bool isDaemon{false};          ///< Whether the process should run as a daemon
+  bool hasTestConsole{false};    ///< Whether there is a test console running in the foreground
+  std::string pathConfigFile;    ///< The path of the configuration file
+  std::string pathConfigFolder;  ///< The path of the configuration folder
+  std::string logFile;           ///< The path of the log file
+};
+}  // namespace app
